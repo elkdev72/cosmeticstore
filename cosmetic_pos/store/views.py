024effce33733461f90173aba514d0e334046b46
elkdev72 <elijahkanyia.developer@gmail.com>
2024-06-01 20:37:40 +0300

    auth

--- cosmetic_pos/store/views.py
+++ cosmetic_pos/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -6,7 +6,23 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Customer, Transaction
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'store/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'store/login.html')
 
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
 def index(request):
     products = Product.objects.all()
     return render(request, 'store/index.html', {'products': products})
@@ -23,3 +39,5 @@ def buy_product(request, product_id):
         return HttpResponseRedirect(reverse('index'))
     customers = Customer.objects.all()
     return render(request, 'store/buy_product.html', {'product': product, 'customers': customers})
+
+
