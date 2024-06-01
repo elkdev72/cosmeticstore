f6e59a914943f57c2d6056bce58352876ce333f8
elkdev72 <elijahkanyia.developer@gmail.com>
2024-06-01 20:38:26 +0300

    auth

--- cosmetic_pos/store/urls.py
+++ cosmetic_pos/store/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('buy/<int:product_id>/', views.buy_product, name='buy_product'),
 ]
