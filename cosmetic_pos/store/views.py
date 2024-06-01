9a2ed97b0062b1ce3ab0d0990b925a40949c8442
elkdev72 <elijahkanyia.developer@gmail.com>
2024-06-02 01:17:32 +0300

    features

--- cosmetic_pos/store/views.py
+++ cosmetic_pos/store/views.py
@@ -27,18 +27,36 @@ def index(request):
     products = Product.objects.all()
     return render(request, 'store/index.html', {'products': products})
 
+@login_required
 def buy_product(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
-        customer_id = request.POST['customer_id']
+        customer_id = request.POST.get('customer_id')
+        customer_name = request.POST.get('customer_name')
         quantity = int(request.POST['quantity'])
-        customer = get_object_or_404(Customer, pk=customer_id)
+
+        if customer_id:
+            customer = get_object_or_404(Customer, pk=customer_id)
+        elif customer_name:
+            first_name, last_name = customer_name.split(' ', 1)
+            customer = Customer.objects.create(first_name=first_name, last_name=last_name)
+        else:
+            # Handle error case where neither customer_id nor customer_name is provided
+            return render(request, 'store/buy_product.html', {
+                'product': product,
+                'customers': Customer.objects.all(),
+                'error': 'Please select a customer or enter a new customer name.'
+            })
+        
         Transaction.objects.create(product=product, customer=customer, quantity=quantity)
         product.stock -= quantity
         product.save()
-        return HttpResponseRedirect(reverse('index'))
+        return redirect('index')
+    
     customers = Customer.objects.all()
     return render(request, 'store/buy_product.html', {'product': product, 'customers': customers})
+
+
 
 
 @login_required
