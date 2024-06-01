bf17876b0fe7b44d1e52a44ea5574147ec9efd60
elkdev72 <elijahkanyia.developer@gmail.com>
2024-06-01 20:41:07 +0300

    update products

--- cosmetic_pos/store/urls.py
+++ cosmetic_pos/store/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('buy/<int:product_id>/', views.buy_product, name='buy_product'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('update_product/<int:product_id>/', views.update_product, name='update_product'),
 ]
