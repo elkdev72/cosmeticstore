bdcc2c24351ec9d1e8c96a872e942438083bf777
elkdev72 <elijahkanyia.developer@gmail.com>
2024-06-01 20:24:58 +0300

    models

--- cosmetic_pos/store/admin.py
+++ cosmetic_pos/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Customer, Transaction
+
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Transaction)
